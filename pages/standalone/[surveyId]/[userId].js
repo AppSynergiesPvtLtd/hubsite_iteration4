6dc37d586f79896741fef8c5fac62c5ac4addc44
baf-03 <bilalahmedfarooqi03@gmail.com>
2025-03-08 04:03:49 +0500

    stand out api

---
+++ pages/standalone/[surveyId]/[userId].js
@@ -0,0 +1,36 @@
+"use client"
+import StandAloneTemplate from "@/components/StandAlone.template"
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const SurveySuccess = () => {
+  const router = useRouter();
+    const {surveyId,userId} = router.query;
+
+    const success= async()=>{
+      try{
+        const apiResp = await axios.get(`${API_BASE_URL}/live-survey/completions/${surveyId}/${userId}`,{
+          headers:{
+            "x-api-key":API_KEY
+          }
+        })
+        console.log(apiResp)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    useEffect(()=>{
+        success()
+    },[])
+  return (
+    <StandAloneTemplate/>
+  )
+}
+
+export default SurveySuccess
+
